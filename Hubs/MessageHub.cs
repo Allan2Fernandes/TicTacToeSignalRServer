68b7bffd94d6295e1f55fc210220fb3c6faacf93
Allan2Fernandes <allan2fernandes@hotmail.com>
2023-04-06 08:23:52 +0200

    Initial commit

---
+++ Hubs/MessageHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace TicTacToeSignalRServer.Hubs
+{
+    public class MessageHub: Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            Debug.WriteLine("Message received at the server: " + message);
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
